27b6a30edaa512cd28ff2cbfebdab6225a6cfd6c
Cương Nguyễn <cuong0993@gmail.com>
2020-10-14 19:30:17 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hitnotes/routes.dart';
 import 'package:path_provider/path_provider.dart';
 
 import 'authentication/authentication_bloc.dart';
@@ -11,6 +10,7 @@ import 'authentication/authentication_event.dart';
 import 'authentication/authentication_state.dart';
 import 'game/game_bloc.dart';
 import 'game/game_widget.dart';
+import 'routes.dart';
 import 'simple_bloc_observer.dart';
 import 'songs/firebase_songs_repository.dart';
 import 'songs/songs_bloc.dart';
