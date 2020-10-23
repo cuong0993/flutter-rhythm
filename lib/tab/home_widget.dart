26efcb5c1b6726ee647047f8be52072a95ab5ebb
Cương Nguyễn <cuong0993@gmail.com>
2020-10-23 11:52:13 +0000

    add

--- lib/tab/home_widget.dart
+++ lib/tab/home_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'level_up_dialog.dart';
 import 'tab_bloc.dart';
 import 'tab_event.dart';
 import 'tab_state.dart';
@@ -17,7 +18,7 @@ class _HomeWidgetState extends State<HomeWidget> {
     BlocProvider.of<TabBloc>(context).userUpLevelStream.listen((event) {
       showDialog<void>(
         context: context,
-        builder: (_) => SuccessDialog(),
+        builder: (_) => LevelUpDialog(),
       );
     });
     super.initState();
@@ -37,44 +38,6 @@ class _HomeWidgetState extends State<HomeWidget> {
           ),
         );
       },
-    );
-  }
-}
-
-class SuccessDialog extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                const Icon(Icons.info),
-                const Flexible(
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Form Submitted Successfully!',
-                      softWrap: true,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            RaisedButton(
-              child: const Text('OK'),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/tab/level_up_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LevelUpDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('AlertDialog'),
+      content: Image(image: AssetImage('images/img_firework.png')),
+      actions: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: const Color(0xFF33b17c),
+            ),
+            child: new Text(
+              'Rate product',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontFamily: 'helvetica_neue_light',
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
