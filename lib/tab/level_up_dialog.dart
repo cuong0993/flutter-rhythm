6dc71d8958a81d2b3f2bbdd6861ae5b9f496fdb8
Cương Nguyễn <cuong0993@gmail.com>
2020-10-24 16:49:20 +0700

    add

--- lib/tab/level_up_dialog.dart
+++ lib/tab/level_up_dialog.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
-import 'base_dialog.dart';
-
 class LevelUpDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BaseDialog(
-      title: '提示',
-      child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: Image(image: AssetImage('assets/images/img_firework.png')),
-      ),
-      onPressed: () {
-       /// NavigatorUtils.push(context, LoginRouter.loginPage, clearStack: true);
-      },
+    return AlertDialog(
+      title: Text('AlertDialog'),
+      content: Image(image: AssetImage('assets/images/img_firework.png')),
+      actions: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: const Color(0xFF33b17c),
+            ),
+            child: Text(
+              'Rate product',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontFamily: 'helvetica_neue_light',
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
