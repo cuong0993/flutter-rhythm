9c3881099984e7a81be0986b2723cced10f350b9
Cương Nguyễn <cuong0993@gmail.com>
2020-10-25 11:54:44 +0700

    add

--- lib/tab/level_up_dialog.dart
+++ lib/tab/level_up_dialog.dart
@@ -1,35 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-// class LevelUpDialog extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return AlertDialog(
-//       title: Text('txt_dialog_level_up_name'.tr()),
-//       content: Image(image: AssetImage('assets/images/img_firework.png')),
-//       actions: [
-//         Expanded(
-//           child: Container(
-//             padding: EdgeInsets.all(16.0),
-//             decoration: BoxDecoration(
-//               color: const Color(0xFF33b17c),
-//             ),
-//             child: Text(
-//               'txt_all_ok'.tr(),
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: 18.0,
-//                 fontFamily: 'helvetica_neue_light',
-//               ),
-//               textAlign: TextAlign.center,
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
 class LevelUpDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -42,6 +13,8 @@ class LevelUpDialog extends StatelessWidget {
         body: Align(
           alignment: Alignment.center,
           child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image(image: AssetImage('assets/images/img_firework.png')),
               Text('txt_dialog_level_up_description'.tr(args: [1.toString()]))
