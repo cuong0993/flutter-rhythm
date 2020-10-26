fe17f63cc850e6cc988528e143902bc44b077009
Cương Nguyễn <cuong0993@gmail.com>
2020-10-26 18:33:35 +0700

    add

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -19,8 +19,80 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
+  static m0(input) => "Reached level ${input}";
+
+  static m1(input) => "Hey, let play Hit Notes ${input}";
+
+  static m2(input) => "Will be unlocked at level ${input} or go premium to unlock now";
+
+  static m3(input) => "${input} days";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static _notInlinedMessages(_) => <String, Function> {
-    "appName" : MessageLookupByLibrary.simpleMessage("Hit Notes")
+    "acoustic_guitar" : MessageLookupByLibrary.simpleMessage("Acoustic Guitar"),
+    "electric_guitar" : MessageLookupByLibrary.simpleMessage("Electric Guitar"),
+    "one_month" : MessageLookupByLibrary.simpleMessage("1 month"),
+    "one_year" : MessageLookupByLibrary.simpleMessage("1 year"),
+    "piano" : MessageLookupByLibrary.simpleMessage("Piano"),
+    "three_month" : MessageLookupByLibrary.simpleMessage("3 months"),
+    "txt_about_invite" : MessageLookupByLibrary.simpleMessage("Invite"),
+    "txt_about_like_facebook" : MessageLookupByLibrary.simpleMessage("Like us on facebook"),
+    "txt_about_rate" : MessageLookupByLibrary.simpleMessage("Rate us"),
+    "txt_about_subscribe_youtube_description" : MessageLookupByLibrary.simpleMessage("Subscribe us on Youtube"),
+    "txt_all_no" : MessageLookupByLibrary.simpleMessage("No"),
+    "txt_all_ok" : MessageLookupByLibrary.simpleMessage("Ok"),
+    "txt_all_songs" : MessageLookupByLibrary.simpleMessage("All songs"),
+    "txt_billing_success" : MessageLookupByLibrary.simpleMessage("Success"),
+    "txt_button_feedback" : MessageLookupByLibrary.simpleMessage("Feedback us"),
+    "txt_button_quit" : MessageLookupByLibrary.simpleMessage("Quit"),
+    "txt_button_restart" : MessageLookupByLibrary.simpleMessage("Play again"),
+    "txt_button_sign_in_facebook" : MessageLookupByLibrary.simpleMessage("Sign in with Facebook"),
+    "txt_button_sign_in_google" : MessageLookupByLibrary.simpleMessage("Sign in with Google"),
+    "txt_dialog_change_to_premium_description" : MessageLookupByLibrary.simpleMessage("Upgrade successful"),
+    "txt_dialog_change_to_premium_name" : MessageLookupByLibrary.simpleMessage("Notify"),
+    "txt_dialog_exit_description" : MessageLookupByLibrary.simpleMessage("Do you want to close?"),
+    "txt_dialog_exit_name" : MessageLookupByLibrary.simpleMessage("Exit Confirmation"),
+    "txt_dialog_level_up_description" : m0,
+    "txt_dialog_level_up_name" : MessageLookupByLibrary.simpleMessage("Congrats"),
+    "txt_dialog_loading_sound_description" : MessageLookupByLibrary.simpleMessage("Loading sound…"),
+    "txt_dialog_logging_in_description" : MessageLookupByLibrary.simpleMessage("Logging in…"),
+    "txt_dialog_purchasing_description" : MessageLookupByLibrary.simpleMessage("Purchasing…"),
+    "txt_dialog_rate_button_remind" : MessageLookupByLibrary.simpleMessage("Remind me later"),
+    "txt_dialog_rate_description" : MessageLookupByLibrary.simpleMessage("If you enjoy using this app, would you mind taking a moment to rate it? It won\'t take more than a minute. Thank you for your support!"),
+    "txt_dialog_rate_name" : MessageLookupByLibrary.simpleMessage("Rate us"),
+    "txt_dialog_reward_name" : MessageLookupByLibrary.simpleMessage("Reward"),
+    "txt_dialog_unlock_instrument" : MessageLookupByLibrary.simpleMessage("Unlock instrument"),
+    "txt_dialog_unlock_song" : MessageLookupByLibrary.simpleMessage("Unlock song"),
+    "txt_dialog_unlocking_song_description" : MessageLookupByLibrary.simpleMessage("Unlocking song…"),
+    "txt_dynamic_link_invite_subject" : MessageLookupByLibrary.simpleMessage("Hit Notes invite"),
+    "txt_en" : MessageLookupByLibrary.simpleMessage("English"),
+    "txt_error_occur" : MessageLookupByLibrary.simpleMessage("Error occurs"),
+    "txt_game_button_continue" : MessageLookupByLibrary.simpleMessage("Continue"),
+    "txt_game_complete_gift" : MessageLookupByLibrary.simpleMessage("Reward"),
+    "txt_game_complete_loading_gift" : MessageLookupByLibrary.simpleMessage("Getting reward…"),
+    "txt_game_complete_name" : MessageLookupByLibrary.simpleMessage("Finished"),
+    "txt_game_dialog_pause_name" : MessageLookupByLibrary.simpleMessage("Paused"),
+    "txt_go_premium" : MessageLookupByLibrary.simpleMessage("Go premium"),
+    "txt_instrument_title_instruments" : MessageLookupByLibrary.simpleMessage("Instruments"),
+    "txt_invite_description" : m1,
+    "txt_ko" : MessageLookupByLibrary.simpleMessage("Korean"),
+    "txt_level" : MessageLookupByLibrary.simpleMessage("Level"),
+    "txt_level_description" : m2,
+    "txt_no_ads" : MessageLookupByLibrary.simpleMessage("No ads"),
+    "txt_not_enough_coins" : MessageLookupByLibrary.simpleMessage("Play to earn enough or go premium to unlock now"),
+    "txt_not_found_purchases" : MessageLookupByLibrary.simpleMessage("Not found purchases"),
+    "txt_page_title_about" : MessageLookupByLibrary.simpleMessage("About"),
+    "txt_premium_in_days" : m3,
+    "txt_purchases_canceled" : MessageLookupByLibrary.simpleMessage("Purchases canceled"),
+    "txt_restore_purchases" : MessageLookupByLibrary.simpleMessage("Restore purchases"),
+    "txt_select_language" : MessageLookupByLibrary.simpleMessage("Select language"),
+    "txt_sign_in" : MessageLookupByLibrary.simpleMessage("Sign in"),
+    "txt_too_early" : MessageLookupByLibrary.simpleMessage("Too early"),
+    "txt_too_late" : MessageLookupByLibrary.simpleMessage("Too late"),
+    "txt_too_many_fingers" : MessageLookupByLibrary.simpleMessage("Too many fingers"),
+    "txt_unlock_all_instruments" : MessageLookupByLibrary.simpleMessage("Unlock all instruments"),
+    "txt_unlock_all_songs" : MessageLookupByLibrary.simpleMessage("Unlock all songs"),
+    "txt_vi" : MessageLookupByLibrary.simpleMessage("Vietnamese"),
+    "txt_zh" : MessageLookupByLibrary.simpleMessage("Chinese")
   };
 }
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -35,13 +35,653 @@ class S {
     return Localizations.of<S>(context, S);
   }
 
-  /// `Hit Notes`
-  String get appName {
+  /// `Like us on facebook`
+  String get txt_about_like_facebook {
     return Intl.message(
-      'Hit Notes',
-      name: 'appName',
+      'Like us on facebook',
+      name: 'txt_about_like_facebook',
       desc: '',
       args: [],
+    );
+  }
+
+  /// `Rate us`
+  String get txt_about_rate {
+    return Intl.message(
+      'Rate us',
+      name: 'txt_about_rate',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Invite`
+  String get txt_about_invite {
+    return Intl.message(
+      'Invite',
+      name: 'txt_about_invite',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Subscribe us on Youtube`
+  String get txt_about_subscribe_youtube_description {
+    return Intl.message(
+      'Subscribe us on Youtube',
+      name: 'txt_about_subscribe_youtube_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `No`
+  String get txt_all_no {
+    return Intl.message(
+      'No',
+      name: 'txt_all_no',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ok`
+  String get txt_all_ok {
+    return Intl.message(
+      'Ok',
+      name: 'txt_all_ok',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Success`
+  String get txt_billing_success {
+    return Intl.message(
+      'Success',
+      name: 'txt_billing_success',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Feedback us`
+  String get txt_button_feedback {
+    return Intl.message(
+      'Feedback us',
+      name: 'txt_button_feedback',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Quit`
+  String get txt_button_quit {
+    return Intl.message(
+      'Quit',
+      name: 'txt_button_quit',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign in with Facebook`
+  String get txt_button_sign_in_facebook {
+    return Intl.message(
+      'Sign in with Facebook',
+      name: 'txt_button_sign_in_facebook',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign in with Google`
+  String get txt_button_sign_in_google {
+    return Intl.message(
+      'Sign in with Google',
+      name: 'txt_button_sign_in_google',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Do you want to close?`
+  String get txt_dialog_exit_description {
+    return Intl.message(
+      'Do you want to close?',
+      name: 'txt_dialog_exit_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Exit Confirmation`
+  String get txt_dialog_exit_name {
+    return Intl.message(
+      'Exit Confirmation',
+      name: 'txt_dialog_exit_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Remind me later`
+  String get txt_dialog_rate_button_remind {
+    return Intl.message(
+      'Remind me later',
+      name: 'txt_dialog_rate_button_remind',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `If you enjoy using this app, would you mind taking a moment to rate it? It won't take more than a minute. Thank you for your support!`
+  String get txt_dialog_rate_description {
+    return Intl.message(
+      'If you enjoy using this app, would you mind taking a moment to rate it? It won\'t take more than a minute. Thank you for your support!',
+      name: 'txt_dialog_rate_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Rate us`
+  String get txt_dialog_rate_name {
+    return Intl.message(
+      'Rate us',
+      name: 'txt_dialog_rate_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Hey, let play Hit Notes {input}`
+  String txt_invite_description(Object input) {
+    return Intl.message(
+      'Hey, let play Hit Notes $input',
+      name: 'txt_invite_description',
+      desc: '',
+      args: [input],
+    );
+  }
+
+  /// `Hit Notes invite`
+  String get txt_dynamic_link_invite_subject {
+    return Intl.message(
+      'Hit Notes invite',
+      name: 'txt_dynamic_link_invite_subject',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Error occurs`
+  String get txt_error_occur {
+    return Intl.message(
+      'Error occurs',
+      name: 'txt_error_occur',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Continue`
+  String get txt_game_button_continue {
+    return Intl.message(
+      'Continue',
+      name: 'txt_game_button_continue',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Finished`
+  String get txt_game_complete_name {
+    return Intl.message(
+      'Finished',
+      name: 'txt_game_complete_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Reward`
+  String get txt_game_complete_gift {
+    return Intl.message(
+      'Reward',
+      name: 'txt_game_complete_gift',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Getting reward…`
+  String get txt_game_complete_loading_gift {
+    return Intl.message(
+      'Getting reward…',
+      name: 'txt_game_complete_loading_gift',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Paused`
+  String get txt_game_dialog_pause_name {
+    return Intl.message(
+      'Paused',
+      name: 'txt_game_dialog_pause_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Instruments`
+  String get txt_instrument_title_instruments {
+    return Intl.message(
+      'Instruments',
+      name: 'txt_instrument_title_instruments',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Go premium`
+  String get txt_go_premium {
+    return Intl.message(
+      'Go premium',
+      name: 'txt_go_premium',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `No ads`
+  String get txt_no_ads {
+    return Intl.message(
+      'No ads',
+      name: 'txt_no_ads',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Unlock all instruments`
+  String get txt_unlock_all_instruments {
+    return Intl.message(
+      'Unlock all instruments',
+      name: 'txt_unlock_all_instruments',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Unlock all songs`
+  String get txt_unlock_all_songs {
+    return Intl.message(
+      'Unlock all songs',
+      name: 'txt_unlock_all_songs',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Restore purchases`
+  String get txt_restore_purchases {
+    return Intl.message(
+      'Restore purchases',
+      name: 'txt_restore_purchases',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `About`
+  String get txt_page_title_about {
+    return Intl.message(
+      'About',
+      name: 'txt_page_title_about',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Reward`
+  String get txt_dialog_reward_name {
+    return Intl.message(
+      'Reward',
+      name: 'txt_dialog_reward_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Notify`
+  String get txt_dialog_change_to_premium_name {
+    return Intl.message(
+      'Notify',
+      name: 'txt_dialog_change_to_premium_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `All songs`
+  String get txt_all_songs {
+    return Intl.message(
+      'All songs',
+      name: 'txt_all_songs',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Level`
+  String get txt_level {
+    return Intl.message(
+      'Level',
+      name: 'txt_level',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Electric Guitar`
+  String get electric_guitar {
+    return Intl.message(
+      'Electric Guitar',
+      name: 'electric_guitar',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Acoustic Guitar`
+  String get acoustic_guitar {
+    return Intl.message(
+      'Acoustic Guitar',
+      name: 'acoustic_guitar',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Piano`
+  String get piano {
+    return Intl.message(
+      'Piano',
+      name: 'piano',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `1 month`
+  String get one_month {
+    return Intl.message(
+      '1 month',
+      name: 'one_month',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `3 months`
+  String get three_month {
+    return Intl.message(
+      '3 months',
+      name: 'three_month',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `1 year`
+  String get one_year {
+    return Intl.message(
+      '1 year',
+      name: 'one_year',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Unlock song`
+  String get txt_dialog_unlock_song {
+    return Intl.message(
+      'Unlock song',
+      name: 'txt_dialog_unlock_song',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Play to earn enough or go premium to unlock now`
+  String get txt_not_enough_coins {
+    return Intl.message(
+      'Play to earn enough or go premium to unlock now',
+      name: 'txt_not_enough_coins',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Purchases canceled`
+  String get txt_purchases_canceled {
+    return Intl.message(
+      'Purchases canceled',
+      name: 'txt_purchases_canceled',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `{input} days`
+  String txt_premium_in_days(Object input) {
+    return Intl.message(
+      '$input days',
+      name: 'txt_premium_in_days',
+      desc: '',
+      args: [input],
+    );
+  }
+
+  /// `Upgrade successful`
+  String get txt_dialog_change_to_premium_description {
+    return Intl.message(
+      'Upgrade successful',
+      name: 'txt_dialog_change_to_premium_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Not found purchases`
+  String get txt_not_found_purchases {
+    return Intl.message(
+      'Not found purchases',
+      name: 'txt_not_found_purchases',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Congrats`
+  String get txt_dialog_level_up_name {
+    return Intl.message(
+      'Congrats',
+      name: 'txt_dialog_level_up_name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Logging in…`
+  String get txt_dialog_logging_in_description {
+    return Intl.message(
+      'Logging in…',
+      name: 'txt_dialog_logging_in_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Loading sound…`
+  String get txt_dialog_loading_sound_description {
+    return Intl.message(
+      'Loading sound…',
+      name: 'txt_dialog_loading_sound_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Purchasing…`
+  String get txt_dialog_purchasing_description {
+    return Intl.message(
+      'Purchasing…',
+      name: 'txt_dialog_purchasing_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Reached level {input}`
+  String txt_dialog_level_up_description(Object input) {
+    return Intl.message(
+      'Reached level $input',
+      name: 'txt_dialog_level_up_description',
+      desc: '',
+      args: [input],
+    );
+  }
+
+  /// `Unlocking song…`
+  String get txt_dialog_unlocking_song_description {
+    return Intl.message(
+      'Unlocking song…',
+      name: 'txt_dialog_unlocking_song_description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `English`
+  String get txt_en {
+    return Intl.message(
+      'English',
+      name: 'txt_en',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Vietnamese`
+  String get txt_vi {
+    return Intl.message(
+      'Vietnamese',
+      name: 'txt_vi',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Chinese`
+  String get txt_zh {
+    return Intl.message(
+      'Chinese',
+      name: 'txt_zh',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Select language`
+  String get txt_select_language {
+    return Intl.message(
+      'Select language',
+      name: 'txt_select_language',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Korean`
+  String get txt_ko {
+    return Intl.message(
+      'Korean',
+      name: 'txt_ko',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Play again`
+  String get txt_button_restart {
+    return Intl.message(
+      'Play again',
+      name: 'txt_button_restart',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign in`
+  String get txt_sign_in {
+    return Intl.message(
+      'Sign in',
+      name: 'txt_sign_in',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Too early`
+  String get txt_too_early {
+    return Intl.message(
+      'Too early',
+      name: 'txt_too_early',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Too late`
+  String get txt_too_late {
+    return Intl.message(
+      'Too late',
+      name: 'txt_too_late',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Too many fingers`
+  String get txt_too_many_fingers {
+    return Intl.message(
+      'Too many fingers',
+      name: 'txt_too_many_fingers',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Unlock instrument`
+  String get txt_dialog_unlock_instrument {
+    return Intl.message(
+      'Unlock instrument',
+      name: 'txt_dialog_unlock_instrument',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Will be unlocked at level {input} or go premium to unlock now`
+  String txt_level_description(Object input) {
+    return Intl.message(
+      'Will be unlocked at level $input or go premium to unlock now',
+      name: 'txt_level_description',
+      desc: '',
+      args: [input],
     );
   }
 }
@@ -52,7 +692,6 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
-      Locale.fromSubtags(languageCode: 'vi'),
     ];
   }
 
