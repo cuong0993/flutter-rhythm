9861675573013d8f18a784c6732e60f1e1a2005e
cuong0993 <cuong0993@gmail.com>
2020-10-28 10:37:34 +0000

    add

--- lib/tab/level_up_dialog.dart
+++ lib/tab/level_up_dialog.dart
@@ -5,34 +5,41 @@ import '../generated/l10n.dart';
 class LevelUpDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          title: Text(S.of(context).txt_dialog_level_up_name),
-        ),
-        body: Align(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(image: AssetImage('assets/images/img_firework.png')),
-              Text(S.of(context).txt_dialog_level_up_description('1'))
-            ],
-          ),
-        ),
-        bottomNavigationBar: Row(
+    return Material(
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(width: 8),
+            Text(S.of(context).txt_dialog_level_up_name),
             Expanded(
-              child: RaisedButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: Text(S.of(context).txt_all_ok),
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(image: AssetImage('assets/images/img_firework.png')),
+                    Text(S.of(context).txt_dialog_level_up_description('1'))
+                  ],
+                ),
               ),
             ),
-            SizedBox(width: 8)
+            Row(
+              children: [
+                SizedBox(width: 8),
+                Expanded(
+                  child: RaisedButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text(S.of(context).txt_all_ok),
+                  ),
+                ),
+                SizedBox(width: 8)
+              ],
+            )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
