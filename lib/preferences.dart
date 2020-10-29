a3a2fa279f28c0a3371a3f2f54cfca135f334d52
cuong0993 <cuong0993@gmail.com>
2020-10-29 10:56:04 +0000

    add

--- lib/preferences.dart
+++ lib/preferences.dart
@@ -3,7 +3,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Preferences {
   static Preferences _instance;
   static SharedPreferences _instance1;
+
   Preferences._internal();
+
   static const KEY_PREFERENCES = 'Preferences';
   static const PREFERENCE_IS_SHOW_RATE_DIALOG_AGAIN = 'IsShowRateDialogAgain';
   static const PREFERENCE_LAUNCH_COUNT = 'LaunchCount';
@@ -16,22 +18,31 @@ class Preferences {
     return _instance;
   }
 
-  bool get isShowRateDialogAgain => _instance1.getBool(PREFERENCE_IS_SHOW_RATE_DIALOG_AGAIN) ?? true;
+  bool get isShowRateDialogAgain =>
+      _instance1.getBool(PREFERENCE_IS_SHOW_RATE_DIALOG_AGAIN) ?? true;
+
   set isShowRateDialogAgain(bool isShowRateDialogAgain) {
-    _instance1.setBool(PREFERENCE_IS_SHOW_RATE_DIALOG_AGAIN, isShowRateDialogAgain);
+    _instance1.setBool(
+        PREFERENCE_IS_SHOW_RATE_DIALOG_AGAIN, isShowRateDialogAgain);
   }
 
   int get launchCount => _instance1.getInt(PREFERENCE_LAUNCH_COUNT) ?? 0;
+
   set launchCount(int launchCount) {
     _instance1.setInt(PREFERENCE_LAUNCH_COUNT, launchCount);
   }
 
-  int get millisecondsFirstLaunch => _instance1.getInt(PREFERENCE_MILLISECONDS_FIRST_LAUNCH) ?? DateTime.now().millisecondsSinceEpoch;
+  int get millisecondsFirstLaunch =>
+      _instance1.getInt(PREFERENCE_MILLISECONDS_FIRST_LAUNCH) ??
+      DateTime.now().millisecondsSinceEpoch;
+
   set millisecondsFirstLaunch(int millisecondsFirstLaunch) {
-    _instance1.setInt(PREFERENCE_MILLISECONDS_FIRST_LAUNCH, millisecondsFirstLaunch);
+    _instance1.setInt(
+        PREFERENCE_MILLISECONDS_FIRST_LAUNCH, millisecondsFirstLaunch);
   }
 
   String get localeName => _instance1.getString(PREFERENCE_LOCALE_NAME) ?? 'en';
+
   set localeName(String localeName) {
     _instance1.setString(PREFERENCE_LOCALE_NAME, localeName);
   }
