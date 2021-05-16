2100ca9218d3676165de0959adb8c26c44e95620
Cương Nguyễn <cuong0993@gmail.com>
2021-05-16 15:21:02 +0700

    Add script to deploy firestore and storage rules

--- synchronize_backend.sh
+++ synchronize_backend.sh
@@ -3,3 +3,6 @@ cd ../functions/functions;
 gcloud functions deploy OnUserSignUp --entry-point functions.OnUserSignUp --runtime java11 --trigger-event providers/firebase.auth/eventTypes/user.create --project=$1 --update-env-vars "GOOGLE_CLOUD_PROJECT=$1" --memory=256MB --timeout=540s;
 gcloud functions deploy OnUserDeleted --entry-point functions.OnUserDeleted --runtime java11 --trigger-event providers/firebase.auth/eventTypes/user.delete --project=$1 --update-env-vars "GOOGLE_CLOUD_PROJECT=$1" --memory=256MB --timeout=540s;
 gcloud functions deploy GetGameReward --entry-point functions.GetGameReward --runtime java11 --trigger-http --allow-unauthenticated --project=$1 --update-env-vars "GOOGLE_CLOUD_PROJECT=$1" --memory=256MB --timeout=540s;
+cd ..;
+firebase deploy --only firestore --project $1;
+firebase deploy --only storage --project $1;
