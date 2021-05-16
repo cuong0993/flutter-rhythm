dea2d443592b3259268410d0abbf4558a9075386
Cương Nguyễn <cuong0993@gmail.com>
2021-05-16 09:57:39 +0700

    Fix songs widget

--- lib/songs/songs_widget.dart
+++ lib/songs/songs_widget.dart
@@ -16,55 +16,63 @@ class SongsWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final songsByTag = watch(songsByTagProvider(_tag));
-    final isLoadingByTag = watch(isLoadingByTagProvider(_tag));
     return songsByTag.when(
-        data: (songsByTag) {
-          return Scrollbar(
-            child: NotificationListener<ScrollEndNotification>(
-              onNotification: (notification) {
-                if (notification.metrics.pixels > 0 &&
-                    notification.metrics.atEdge) {
-                  context
-                      .read(songsByTagProvider(_tag).notifier)
-                      .loadMoreSongs();
-                }
-                return true;
-              },
-              child: ListView.separated(
-                itemCount: isLoadingByTag.state
-                    ? songsByTag.length + 1
-                    : songsByTag.length,
-                itemBuilder: (context, index) {
-                  if (index == songsByTag.length) {
-                    return const Center(
-                      child: SizedBox(
-                        height: 32,
-                        width: 32,
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: CircularProgressIndicator(),
-                        ),
+        data: (songsByTag) => Column(
+              children: [
+                Expanded(
+                  child: Scrollbar(
+                    child: NotificationListener<ScrollEndNotification>(
+                      onNotification: (notification) {
+                        if (notification.metrics.pixels > 0 &&
+                            notification.metrics.atEdge) {
+                          context
+                              .read(songsByTagProvider(_tag).notifier)
+                              .loadMoreSongs();
+                        }
+                        return true;
+                      },
+                      child: ListView.separated(
+                        itemCount: songsByTag.length,
+                        itemBuilder: (context, index) {
+                          final song = songsByTag[index];
+                          return SongWidget(
+                            song: song,
+                            onTap: () async {
+                              await Navigator.pushNamed(
+                                  context, Routes.gameConfig,
+                                  arguments: song);
+                            },
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return const Divider(
+                            height: 4,
+                          );
+                        },
                       ),
-                    );
-                  }
-                  final song = songsByTag[index];
-                  return SongWidget(
-                    song: song,
-                    onTap: () async {
-                      await Navigator.pushNamed(context, Routes.gameConfig,
-                          arguments: song);
-                    },
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const Divider(
-                    height: 4,
-                  );
-                },
-              ),
+                    ),
+                  ),
+                ),
+                Consumer(
+                  builder: (context, watch, child) {
+                    final isLoadingByTag =
+                        watch(isLoadingByTagProvider(_tag)).state;
+                    return isLoadingByTag
+                        ? const Center(
+                            child: SizedBox(
+                              height: 32,
+                              width: 32,
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: CircularProgressIndicator(),
+                              ),
+                            ),
+                          )
+                        : Container();
+                  },
+                )
+              ],
             ),
-          );
-        },
         loading: () => LoadingWidget(),
         error: (_, __) => LoadingWidget());
   }
