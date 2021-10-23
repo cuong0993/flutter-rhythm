a52eabe87bc8b7f4e4743c6f1547ec6460622c2e
Cương Nguyễn <cuong0993@gmail.com>
2021-10-23 12:25:00 +0700

    Remove flutter_flavorizr by dart defines

--- lib/app.dart
+++ lib/app.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'flavors.dart';
 import 'locale/locale_controller.dart';
 import 'router/router.dart';
 import 'theme/theme_controller.dart';
@@ -20,7 +19,6 @@ class App extends ConsumerWidget {
     final themeMode = ref.watch(themeModeProvider);
     final locale = ref.watch(localeProvider);
     return MaterialApp.router(
-      title: F.title,
       debugShowCheckedModeBanner: false,
       locale: locale,
       localizationsDelegates: L10n.localizationsDelegates,
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-// ignore_for_file: file_names
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
-import 'flavors.dart';
 import 'preferences.dart';
 
 class LogProviderObserver extends ProviderObserver {
@@ -56,21 +55,31 @@ Dispose
 }
 
 Future<void> main() async {
-  F.appFlavor = Flavor.dev;
   WidgetsFlutterBinding.ensureInitialized();
   final sharedPreferences = await SharedPreferences.getInstance();
   await Firebase.initializeApp();
-  Logger.root.onRecord.listen((record) {
-    // ignore: avoid_print
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-  runApp(
-    ProviderScope(
-      observers: [LogProviderObserver()],
-      overrides: [
-        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
-      ],
-      child: App(),
-    ),
-  );
+  if (kReleaseMode) {
+    runApp(
+      ProviderScope(
+        overrides: [
+          sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+        ],
+        child: App(),
+      ),
+    );
+  } else {
+    Logger.root.onRecord.listen((record) {
+      // ignore: avoid_print
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    });
+    runApp(
+      ProviderScope(
+        observers: [LogProviderObserver()],
+        overrides: [
+          sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+        ],
+        child: App(),
+      ),
+    );
+  }
 }
